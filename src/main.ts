2c1ee4f9926de2b9cdb815dee0e0ff7b2f1b471c
apricotxapril <auay.smiley@icloud.com>
2022-07-11 15:40:20 +0700

    config firebase

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,26 @@ import App from './App.vue'
 import router from './router'
 import './scss/main.scss'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBbFd2BulpUP3IU95OSbDgc7vlDWAtcwIU',
+  authDomain: 'vue-firebase-practice-1398e.firebaseapp.com',
+  projectId: 'vue-firebase-practice-1398e',
+  storageBucket: 'vue-firebase-practice-1398e.appspot.com',
+  messagingSenderId: '1008668249407',
+  appId: '1:1008668249407:web:f58957dcc1d6b3f25cd78a',
+  measurementId: 'G-RCFF9FZEWD',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
+
 createApp(App).use(router).mount('#app')
