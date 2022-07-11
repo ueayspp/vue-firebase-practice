55af10570f4f38eabf07fcffd163238ea957ae30
apricotxapril <auay.smiley@icloud.com>
2022-07-11 16:00:44 +0700

    added Home.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <form @submit.prevent="login">
+      <h2>Login</h2>
+      <input type="email" placeholder="Email address..." v-model="email" />
+      <input type="password" placeholder="password..." v-model="password" />
+      <button class="text-white px-4 py-1 bg-green-500 rounded-full" type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      email: '',
+      password: '',
+    }
+  },
+  methods: {
+    login() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(() => {
+          alert('Successfully logged in')
+          this.$router.push('/dashboard')
+        })
+        .catch((error) => {
+          alert(error.message)
+        })
+    },
+  },
+}
+</script>
