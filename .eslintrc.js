548471530ccbfe1bb0cb0c14566cde27ec9890ec
apricotxapril <auay.smiley@icloud.com>
2022-07-11 13:43:21 +0700

    config code formatter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,45 @@
 module.exports = {
   root: true,
   env: {
+    browser: true,
     node: true,
   },
   extends: [
-    "plugin:vue/vue3-essential",
+    "plugin:vue/vue3-recommended",
     "eslint:recommended",
     "@vue/typescript/recommended",
-    "plugin:prettier/recommended",
+    "@vue/prettier",
+    "@vue/prettier/@typescript-eslint",
   ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
+  plugins: ["@typescript-eslint"],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    // override/add rules settings here, such as:
+    "max-len": [
+      "warn",
+      {
+        code: 100,
+        ignoreComments: true,
+        ignoreTrailingComments: true,
+        ignorePattern: "^\\s*import",
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+      },
+    ],
+    "vue/no-unused-vars": "warn",
+    "@typescript-eslint/explicit-module-boundary-types": [
+      "off",
+      {
+        allowedNames: ["ignoredFunctionName", "ignoredMethodName"],
+      },
+    ],
+    "vue/valid-v-model": "off",
+    "vue/no-v-html": "off",
+    quotes: ["warn", "single"],
+    semi: ["warn", "never"],
+    "vue/multi-word-component-names": 0,
+  },
+  globals: {
+    defineProps: "readonly",
+    defineEmits: "readonly",
   },
 };
